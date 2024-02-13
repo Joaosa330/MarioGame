58c4cc9c65db3a050177d32e3e20801c3251ef0b
Joao Sa <jsa@pop-os.Home>
2024-02-13 17:35:00 +0000

    new structure

--- src/main/java/MarioGame.java
+++ src/main/java/MarioGame.java
@@ -1,50 +1,71 @@
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
 
 public class MarioGame {
+    private final Terminal terminal;
+    private int marioX;
+    private int marioY;
+
+    public MarioGame() throws IOException {
+        terminal = new DefaultTerminalFactory().createTerminal();
+        terminal.setCursorVisible(false);
+        marioX = 5;
+        marioY = 5;
+    }
+
+    public void run() throws IOException {
+        boolean running = true;
+        while (running) {
+            terminal.clearScreen();
+            renderMario();
+            terminal.flush();
+
+            KeyStroke keyStroke = terminal.pollInput();
+            if (keyStroke != null) {
+                processInput(keyStroke);
+                if (keyStroke.getKeyType() == KeyType.Escape) {
+                    running = false;
+                }
+            }
+        }
+    }
+
+    private void renderMario() throws IOException {
+        terminal.setCursorPosition(marioX, marioY);
+        terminal.setForegroundColor(TextColor.ANSI.RED);
+        terminal.putCharacter('M');
+    }
+
+    private void processInput(KeyStroke keyStroke) {
+        switch (keyStroke.getKeyType()) {
+            case ArrowUp:
+                marioY--;
+                break;
+            case ArrowDown:
+                marioY++;
+                break;
+            case ArrowLeft:
+                marioX--;
+                break;
+            case ArrowRight:
+                marioX++;
+                break;
+            default:
+                break;
+        }
+    }
 
     public static void main(String[] args) {
-        Terminal terminal = TerminalFacade.createTerminal();
-        terminal.enterPrivateMode();
-
-        int marioX = 5;
-        int marioY = 5;
-
-        while (true) {
-            terminal.moveCursor(marioX, marioY);
-            terminal.putCharacter('M');
-
-            Key key;
-            do {
-                try {
-                    Thread.sleep(5);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                key = terminal.readInput();
-            } while (key == null);
-
-            terminal.moveCursor(marioX, marioY);
-            terminal.putCharacter(' ');
-
-            switch (key.getKind()) {
-                case ArrowUp:
-                    marioY--;
-                    break;
-                case ArrowDown:
-                    marioY++;
-                    break;
-                case ArrowLeft:
-                    marioX--;
-                    break;
-                case ArrowRight:
-                    marioX++;
-                    break;
-                case Escape:
-                    terminal.exitPrivateMode();
-                    return;
-            }
+        try {
+            MarioGame game = new MarioGame();
+            game.run();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
